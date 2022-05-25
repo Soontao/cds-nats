deb0f4e5165543e72145625bdda9a8443e9f5e3a
Theo Sun <theo.sun.github@outlook.com>
2022-05-25 13:44:15 +0800

    feat: connect to nc

--- src/NATSMessagingService.ts
+++ src/NATSMessagingService.ts
@@ -1,5 +1,17 @@
-import { cwdRequireCDS } from "cds-internal-tool";
+import { cwdRequireCDS, Logger } from "cds-internal-tool";
+import { connect as connectNats, NatsConnection } from "nats";
 
 export class NATSMessagingService extends cwdRequireCDS().MessagingService {
+
+  private logger!: Logger;
+
+  private nc!: NatsConnection;
+
+  async init(): Promise<any> {
+    await super.init();
+    const cds = cwdRequireCDS();
+    this.logger = cds.log("nats|messaging");
+    this.nc = await connectNats(this.options);
+  }
 
 }
