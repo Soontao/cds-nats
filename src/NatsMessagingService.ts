f8900a9768c5a755e9fbd86a0d828d5d6ffa729b
Theo Sun <theo.sun@outlook.com>
2022-06-23 15:57:01 +0800

    refactor: messaging service

--- src/NatsMessagingService.ts
+++ src/NatsMessagingService.ts
@@ -1,5 +1,5 @@
 import { ApplicationService, cwdRequireCDS, Definition, Service, TransactionMix } from "cds-internal-tool";
-import { Msg, Subscription } from "nats";
+import { Msg, MsgHdrs, Subscription } from "nats";
 import { FatalError } from "./errors";
 import { NatsService } from "./NatsService";
 import { extractUserAndTenant, toHeader, toNatsHeaders } from "./utils";
@@ -58,8 +58,9 @@ export class NatsMessagingService extends NatsService {
   private async _handleInboundMessages(srv: ApplicationService, def: Definition, sub: Subscription) {
     // use the service local event name, otherwise, framework could not found the handlers
     const event = def.name.substring(srv.name.length + 1);
+    const subject = sub.getSubject();
     for await (const msg of sub) {
-      await this._handleInboundMessage(msg, def, srv, sub, event);
+      await this._handleInboundMessage(msg, def, srv, subject, event);
     }
   }
 
@@ -69,10 +70,16 @@ export class NatsMessagingService extends NatsService {
    * @param msg 
    * @param def 
    * @param srv 
-   * @param sub 
+   * @param subject 
    * @param event 
    */
-  private async _handleInboundMessage(msg: Msg, def: Definition, srv: ApplicationService, sub: Subscription, event: string) {
+  private async _handleInboundMessage(
+    msg: Msg,
+    def: Definition,
+    srv: ApplicationService,
+    subject: string,
+    event: string
+  ) {
     try {
       const cds = cwdRequireCDS();
       const data = this.codec.decode(msg.data);
@@ -82,7 +89,7 @@ export class NatsMessagingService extends NatsService {
       this.logger.debug(
         "receive event", def.name,
         "for service", srv.name,
-        "subject is", sub.getSubject(),
+        "subject is", subject,
         "tenant is", tenant
       );
       const txSrv: ApplicationService & TransactionMix = cds.context = srv.tx({ tenant, user }) as any;
@@ -112,25 +119,36 @@ export class NatsMessagingService extends NatsService {
   public async emit(event: string, data?: any, headers?: any): Promise<this>;
 
   public async emit(event: any, data?: any, headers?: any): Promise<this> {
-
     // outbound emit
-
     const msg: any = typeof event === "object" ? event : { event, data, headers };
-
-    const target = this._prepareTarget(msg.event, false);
-
+    const subject = this._prepareSubject(msg.event, false);
     const msgHeaders = toNatsHeaders(msg.headers, msg.event);
-
-    this.logger.debug("emit subject", target, "with data", msg.data, "headers", msg.headers);
-
-    this.nc.publish(target, this.codec.encode(msg.data), { headers: msgHeaders });
-
-    await this.nc.flush();
-
+    this.logger.debug(
+      "emit subject", subject,
+      "with data", msg.data,
+      "headers", msg.headers
+    );
+    await this._publish(subject, msg?.data, msgHeaders);
     return this;
 
   }
 
+
+  /**
+   * Nats internal publish function
+   * 
+   * @param subject 
+   * @param data 
+   * @param msgHeaders 
+   */
+  private async _publish(subject: string, data?: any, msgHeaders?: MsgHdrs) {
+    this.nc.publish(
+      subject,
+      this.codec.encode(data),
+      { headers: msgHeaders }
+    );
+    await this.nc.flush();
+  }
 
   /**
    * prepare target (nats subject) for publisher and listener
@@ -139,7 +157,7 @@ export class NatsMessagingService extends NatsService {
    * @param inbound 
    * @returns 
    */
-  private _prepareTarget(queueOrTopic: string, inbound: boolean) {
+  private _prepareSubject(queueOrTopic: string, inbound: boolean) {
     let res = queueOrTopic;
     // TODO: transform invalid name
     if (!inbound && this.options.publishPrefix) res = this.options.publishPrefix + res;
@@ -163,24 +181,24 @@ export class NatsMessagingService extends NatsService {
 
     // with empty @queue annotation
     if (queueName === true) {
-      queueName = eventDef.name
+      queueName = eventDef.name;
     }
 
     // with empty @topic annotation
     if (topicName === true) {
-      topicName = eventDef.name
+      topicName = eventDef.name;
     }
 
     let options: { target: string, options?: any } = undefined as any;
     if (queueName !== undefined) {
-      const normalizedQueueName = this._prepareTarget(queueName, true);
+      const normalizedQueueName = this._prepareSubject(queueName, true);
       options = {
         target: normalizedQueueName,
         options: { queue: normalizedQueueName }
       };
     }
     if (topicName !== undefined) {
-      options = { target: this._prepareTarget(topicName, true) };
+      options = { target: this._prepareSubject(topicName, true) };
     }
 
     return options;
